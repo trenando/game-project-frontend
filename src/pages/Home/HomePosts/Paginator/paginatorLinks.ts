453f6f26036fc386d6050b3fe37add72e22784f2
trenando <48501601+trenando@users.noreply.github.com>
2021-03-17 12:37:46 +0300

    unsaved pagination file

--- src/pages/Home/HomePosts/Paginator/paginatorLinks.ts
+++ src/pages/Home/HomePosts/Paginator/paginatorLinks.ts
@@ -56,8 +56,13 @@ export const paginatorLinks: PaginatorLinks = (
   const pages: Array<number> = [];
   const allPosts: number = Math.ceil(allPostsCount / postsPerPage);
   for (let i = 1; i <= allPosts; i++) pages.push(i);
-  if (allPosts > 10) {
-    const buttons: Array<string | number> = sortLinksArray(currentPage, pageRange, pages, rangeButtons);
+  if (allPosts > 4 + pageRange + rangeButtons) {
+    const buttons: Array<string | number> = sortLinksArray(
+      currentPage,
+      pageRange,
+      pages,
+      rangeButtons
+    );
     return buttons;
   } else {
     return pages;
